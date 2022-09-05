3b9a1d058b03a7b13b5491a30d4abf1268ab19d7
mathias <mathias.20.1998@gmail.com>
2022-09-04 22:43:57 -0300

    asd

--- contablito.js
+++ contablito.js
@@ -22,11 +22,11 @@ d.addEventListener('submit',(e)=>{
     let ivaMinimo = parseInt($minimo.value * monto) / 100;
     let ivaExento = parseInt($exento.value * monto) / 100;
     
-    function insert(operacion,tipoDeIva){
+    function insertVenta(tipoDeIva){
         $transaciones[0].innerHTML+= `
         <tr>
         <td>${$description.value}</td>
-        <td>${operacion.value}</td>
+        <td>${$venta.value}</td>
         <td class='dayTotalSell'>${$inputtotal.value}</td>
         <td>${tipoDeIva}</td>
         <td>${tipoDeIva + monto}</td>
@@ -46,19 +46,27 @@ function totalVentas() {
 }
 
 if($venta.checked && $basico.checked && !$inputtotal.value == ''){ 
-   insert($venta,ivaBasico)
+    insertVenta(ivaBasico)
    totalVentas()
 }
-
 if($venta.checked && $minimo.checked && !$inputtotal.value == ''){
-    insert($venta,ivaMinimo)
+    insertVenta(ivaMinimo)
     totalVentas()
 }
-
-
 if($venta.checked && $exento.checked && !$inputtotal.value == ''){
-    insert($venta,ivaExento)
+    insertVenta(ivaExento)
     totalVentas()
+}
+function insertCompra(tipoDeIva){
+    $transaciones[0].innerHTML+= `
+    <tr>
+    <td>${$description.value}</td>
+    <td>${$compra.value}</td>
+    <td class='dayTotalBuy'>${$inputtotal.value}</td>
+    <td>${tipoDeIva}</td>
+    <td>${tipoDeIva + monto}</td>
+    </tr>
+    `
 }
 
 function totalCompras() {
@@ -68,20 +76,20 @@ function totalCompras() {
     r.push(s);
     }
     let result = r.map(i=>Number(i))
-    result = result.reduce((a, b) => a + b, 0);
+    result = [...result].reduce((a, b) => a + b, 0);
     $totalbuy.innerHTML = `Total Compras:$${result}`
 }
 
 if($compra.checked && $basico.checked && !$inputtotal.value == ''){
-    insert($compra,ivaBasico)
+    insertCompra(ivaBasico)
     totalCompras()
 }
 if($compra.checked && $minimo.checked && !$inputtotal.value == ''){
-    insert($compra,ivaMinimo)
+    insertCompra(ivaMinimo)
     totalCompras()
 }
 if($compra.checked && $exento.checked && !$inputtotal.value == ''){
-    insert($compra,ivaExento)
+    insertCompra(ivaExento)
     totalCompras()
 }
 if($inputtotal.value == ''){
